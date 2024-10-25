3d9183cf969efa19f9ada2b4015f7d879a7cac13
wjdwodnd93 <jjw_0609@naver.com>
2024-10-25 22:24:34 +0900

    input값 상태 제어

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import {useState} from "react";
+
 function App() {
   // const [count, setCount] = useState(0)
+    const [inputText, setInputText] = useState('');
 
-  return (
+    const handleInput = (event) => {
+        console.log(event);
+        const value = event.target.value;
+        setInputText(value);
+    }
+
+    return (
     <div>
         <h1>TODO 앱</h1>
         <div>
-            <input type="text"/>
+            <input type="text" value={inputText} onChange={handleInput} />
             <button>add</button>
         </div>
     </div>
-  )
+    )
 }
 
 export default App
