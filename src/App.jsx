dddd68ca5fdef3a083aa02553a99d46bc58d86b9
wjdwodnd93 <jjw_0609@naver.com>
2024-10-25 22:00:22 +0900

    jsx input

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
   // const [count, setCount] = useState(0)
-    const a = 10;
 
   return (
     <div>
-        {a}
+        <h1>TODO 앱</h1>
+        <div>
+            <input type="text"/>
+            <button>add</button>
+        </div>
     </div>
   )
 }
