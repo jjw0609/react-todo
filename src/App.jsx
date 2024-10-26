569e7fdab84b53c07381e46c297bfc63bc21d122
wjdwodnd93 <jjw_0609@naver.com>
2024-10-26 23:16:56 +0900

    할 일 저장 기능

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ function App() {
         setInputText(value);
     }
 
+    const handleClick = () => {
+        console.log('clicked');
+        localStorage.setItem(inputText, inputText);
+    }
+
     return (
     <div>
         <h1>TODO 앱</h1>
         <div>
             <input type="text" value={inputText} onChange={handleInput} />
-            <button>add</button>
+            <button onClick={handleClick}>add</button>
         </div>
     </div>
     )
