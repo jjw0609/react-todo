1bf747f92257f3662fa8f8707116a801053b5a73
wjdwodnd93 <jjw_0609@naver.com>
2024-11-15 20:08:04 +0900

    코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,18 @@ import TodoInput from "./components/TodoInput.jsx";
 import TodoList from "./components/TodoList.jsx";
 
 
-// function fetchTodos() {
-//     const result = [];
-//     for (let i = 0; i<localStorage.length ; i++) {
-//         const value = localStorage.key(i);
-//         result.push(value);
-//     }
-//
-//     return result;
-// }
+function fetchTodos() {
+    const result = [];
+    for (let i = 0; i<localStorage.length ; i++) {
+        const value = localStorage.key(i);
+        result.push(value);
+    }
+
+    return result;
+}
 
 function App() {
     const [todos, setTodos] = useState(fetchTodos());
-
-    // const handleInput = (event) => {
-    //     console.log(event);
-    //     const value = event.target.value;
-    //     setInputText(value);
-    // }
-
-    // const handleClick = () => {
-    //     console.log('clicked');
-    //     localStorage.setItem(inputText, inputText);
-    //
-    //     setTodos((currentTodos) => {
-    //         return [...currentTodos, inputText];
-    //     })
-    //     setInputText('');
-    // }
 
     const addTodo = (todo) => {
         console.log('clicked');
@@ -40,11 +24,9 @@ function App() {
         setTodos((currentTodos) => {
             return [...currentTodos, todo];
         })
-        // setInputText('');
     }
 
-    const handleRemove = (todo) => {
-        // todos.splice(index, 1);
+    const removeTodo = (todo) => {
         const result = todos.filter(todoItem => todoItem !== todo)
         setTodos(result);
         localStorage.removeItem(todo);
@@ -54,7 +36,7 @@ function App() {
         <div>
             <TodoHeader />
             <TodoInput onTodoAdd={addTodo} />
-            <TodoList todos={todos} onTodoRemove={handleRemove} />
+            <TodoList todos={todos} onTodoRemove={removeTodo} />
         </div>
     )
 }
